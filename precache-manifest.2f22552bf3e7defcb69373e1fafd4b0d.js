0525ad13d82b87f920167c32a3fdc4ba00c87f52
Eric Fu <eric.fu1009@gmail.com>
2020-07-15 04:15:23 -0600

    Updates

---
+++ precache-manifest.2f22552bf3e7defcb69373e1fafd4b0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28a5dd9fc88d78518210e553f46a3f0",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "06a99e4763572a16d02b",
+    "url": "/quizapp/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "df1ebe83b2a496032ee8",
+    "url": "/quizapp/static/js/2.1ba14ca9.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/quizapp/static/js/2.1ba14ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06a99e4763572a16d02b",
+    "url": "/quizapp/static/js/main.6038acea.chunk.js"
+  },
+  {
+    "revision": "a6508cc90f14bba2473a",
+    "url": "/quizapp/static/js/runtime-main.ae6c255c.js"
+  }
+]);
